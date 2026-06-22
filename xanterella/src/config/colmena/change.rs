8c9f7dfc37a2f0356436c9bb7e94769cbd48a009
Xeravus <cato.jenisch@gmail.com>
2026-06-22 18:44:20 +0200

    Added Write Hosts & Write Hosts Config Functions

--- xanterella/src/config/colmena/change.rs
+++ xanterella/src/config/colmena/change.rs
@@ -1,12 +1,14 @@
 use log::{info, error};
 
-use crate::config::colmena::parse::*;
+use std::fs;
 
-/*
-pub fn write_hosts(file: ColmenaFile) {
-    let
+use crate::config::colmena::parse::*;
+use crate::utils::get::*;
+
+pub fn write_hosts(content: &str, file: ColmenaFile) {
+    let replaced_content = content.replace(&colmena_split_marker(&content), &write_hosts_config(file));
+    let _ = fs::write(get_path(Paths::Colmena), replaced_content);
 }
-*/
 
 pub fn sort_hosts(hosts: ColmenaFile) -> ColmenaFile {
     let mut output: Vec<ColmenaHost> = hosts.hosts;
@@ -14,6 +16,14 @@ pub fn sort_hosts(hosts: ColmenaFile) -> ColmenaFile {
     ColmenaFile {
         hosts: output,
     }
+}
+
+pub fn write_hosts_config(file: ColmenaFile) -> String {
+    let mut hosts: Vec<String> = vec![];
+    for i in file.hosts {
+        hosts.push(write_host_config(i))
+    }
+    hosts.join("\n")
 }
 
 pub fn write_host_config(host: ColmenaHost) -> String {
