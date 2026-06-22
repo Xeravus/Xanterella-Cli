721f07904ff7dce74cca4a0bcdca669fd4825cb8
Xeravus <cato.jenisch@gmail.com>
2026-06-22 13:33:36 +0200

    Working on write Host modul

--- xanterella/src/config/colmena/change.rs
+++ xanterella/src/config/colmena/change.rs
@@ -5,3 +5,42 @@ pub fn write_hosts(file: ColmenaFile) {
 
 pub fn sort_hosts(hosts: ColmenaFile) -> ColmenaFile {
 }
+
+pub fn write_host_config(host: ColmenaHosts) -> String {
+    let start_string = format!("
+    {} = {{
+    deployment = {{
+    targetHost = '{}';
+    ", host.name, host.ip);
+    let middle_string = if host.remotebuilder {
+        format!("
+        targetHost = null
+        allowLocalDeployment = true;
+        buildOnTarget = true;
+        ") 
+    } else {
+        format!("
+        targetHost = taruser;
+        buildOnTarget = false;
+        keys = commonSSHKeys;
+        ")
+    }
+    let end_string = format!("
+        imports = [
+        {}
+        ];", for i in host.imports { 
+        format!("{}\n", i)
+    });
+    let final_string = format!("
+    {}
+    {}
+    }};
+    {}
+    }};
+           ", 
+           start_string,
+           middle_string,
+           end_string,
+     );
+    final_string
+}
--- xanterella/src/config/colmena/mod.rs
+++ xanterella/src/config/colmena/mod.rs
@@ -1,2 +1,3 @@
 pub mod parse;
 pub mod add;
+//pub mod change;
