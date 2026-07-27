4ce9dfaa433735add198520f9dd2ae821f314f30
Xeravus <cato.jenisch@gmail.com>
2026-07-27 17:59:50 +0200

    Refactor

---
+++ xanterella/src/oop/result.rs
@@ -0,0 +1,86 @@
+pub enum EventsFaild {
+    Failed(String),
+    FailedCmd(String),
+
+    Ping(String),
+    PingSsh(String),
+
+    CheckNix,
+    
+    GetHardware,
+
+    GitCommit,
+    GitCheckout,
+    GitMerge,
+    GitPr,
+    GitDiff,
+
+    PartEfi,
+    PartRoot,
+    FormatEfi,
+    FormatRoot,
+    CreateBootDir,
+    MountBoot,
+    MountRoot,
+}
+
+pub enum Events {
+    RunPing(String),
+    OkPing(String),
+
+    RunPingSsh(String),
+    OkPingSsh(String),
+
+    RunCheckNix,
+    OkCheckNix, 
+
+    RunGetHardware(String),
+    OkGetHardware(String),
+    //
+    RunGitCommit(String),
+    OkGitCommit(String),
+
+    RunGitCheckout(Branches),
+    OkGitCheckout(Branches),
+
+    RunGitMerge,
+    OkGitMerge,
+
+    RunGitCheckoutCreate(Branches),
+    OkGitCheckoutCreate(Branches),
+
+    RunGitPr(PrType),
+    OkGitPr(PrType),
+
+    RunGitDiff,
+    OkGitDiff,
+    //
+    RunRemoteIntegration(String),
+    OkRemoteIntegration(String),
+
+    RunRemoteInstall(String),
+    OkRemoteInstall(String),
+    //
+    RunPartEfi,
+    OkPartEfi,
+
+    RunPartRoot,
+    OkPartRoot,
+
+    RunFormatEfi,
+    OkFormatEfi,
+
+    RunFormatRoot,
+    OkFormatRoot,
+
+    RunCreateBootDir,
+    OkCreateBootDir,
+
+    RunMountBoot,
+    OkMountBoot,
+
+    RunMountRoot,
+    OkMountRoot,
+    //
+
+}
