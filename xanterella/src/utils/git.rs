4635c60d2638ac589d9f27cd2e3275c35d05c45f
Xeravus <cato.jenisch@gmail.com>
2026-07-27 23:39:52 +0200

    Fix

--- xanterella/src/utils/git.rs
+++ xanterella/src/utils/git.rs
@@ -2,11 +2,13 @@ use crate::prelude::*;
 
 use crate::utils::get::*;
 
+#[derive(Debug, Clone)]
 pub enum Branches {
     Main,
     Xanterella,
 }
     
+#[derive(Debug, Clone)]
 pub enum PrType {
     AddHost(String),
     RemoveHost(String),
@@ -92,7 +94,7 @@ impl Git for Xanterella {
     }
 
     fn git_pr(&mut self, pr: PrType) -> Result<(), EventsFailed> {
-        self.log_event(Events::RunGitPr(pr));
+        self.log_event(Events::RunGitPr(pr.clone()));
 
         let extra_part = "This is an automatic generated Pull Request";
         let title = match pr {
@@ -119,7 +121,7 @@ impl Git for Xanterella {
             return Err(EventsFailed::GitPr);
         };
 
-        self.log_event(Events::OkGitPr(pr));
+        self.log_event(Events::OkGitPr(pr.clone()));
         Ok(())
     }
 }
