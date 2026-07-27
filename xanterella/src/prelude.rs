c3616bef7c79d799935558fcc488f22bee0b0e53
Xeravus <cato.jenisch@gmail.com>
2026-07-27 23:03:05 +0200

    Refactor: Add Prelude

---
+++ xanterella/src/prelude.rs
@@ -0,0 +1,11 @@
+pub use std::process::Command;
+pub use std::path::PathBuf;
+pub use std::fs;
+
+pub use serde::Serialize;
+pub use serde::Deserialize;
+
+pub use crate::xanterella::oop::core::*;
+pub use crate::xanterella::oop::result::*;
+
+pub use crate::xanterella::utils::get::*;
