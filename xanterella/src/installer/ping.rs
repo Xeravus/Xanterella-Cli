0e02268f71908837b0815515280f675c4a759756
Xeravus <cato.jenisch@gmail.com>
2026-07-27 22:05:34 +0200

    Refactor

--- xanterella/src/installer/ping.rs
+++ xanterella/src/installer/ping.rs
@@ -1,10 +1,9 @@
-pub trait Remote {
+pub trait Ping {
     fn ping(&self) -> Result<(), EventsFailed>;
     fn ping_ssh(&self) -> Result<(), EventsFailed>;
-    fn get_hardware(&self) -> Result<String, EventsFailed>;
 }
 
-impl Remote for Xanterella {
+impl Ping for Xanterella {
     fn ping(&self) -> Result<(), EventsFailed> {
         self.log_event(Events::RunPing, &self.ip.clone()); 
 
@@ -36,22 +35,5 @@ impl Remote for Xanterella {
 
         self.log_event(Events::OkPingSsh, &self.ip.clone());
         Ok()
-    }
-
-    fn get_hardware(&self) -> Result<String, EventsFailed> {
-        self.log_event(Events::RunGetHardware(&self.ip.clone()));
-
-        let cmd = Command::new("ssh")
-            .args(self.get_sshstring(User::Root))
-            .args(["nixos-generate-config", "--no-filesystem", "--show-hardware-config"])
-            .output()
-            .map_err(|err| EventsFailed::FailedCmd(err))?;
-
-        if !cmd.status.success() {
-            return Err(EventsFailed::GetHardware(&self.ip.clone()));
-        }
-
-        self.log_event(Events::OkGetHardware(&self.ip.clone()));
-        Ok(String::from_utf8_lossy(&cmd.stdout))
     }
 }
