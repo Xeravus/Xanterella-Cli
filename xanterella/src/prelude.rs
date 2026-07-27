cd9d7aa2975e6161133417f56b5ee2fcaffc9483
Xeravus <cato.jenisch@gmail.com>
2026-07-27 23:18:32 +0200

    Fix

--- xanterella/src/prelude.rs
+++ xanterella/src/prelude.rs
@@ -1,11 +1,18 @@
 pub use std::process::Command;
 pub use std::path::PathBuf;
 pub use std::fs;
+pub use std::collections::HashMap;
+pub use std::collections::HashSet;
 
 pub use serde::Serialize;
 pub use serde::Deserialize;
 
+pub use crate::xanterella::oop::core::Xanterella;
 pub use crate::xanterella::oop::core::*;
-pub use crate::xanterella::oop::result::*;
-
 pub use crate::xanterella::utils::get::*;
+
+// Enums
+pub use crate::xanterella::oop::result::Events;
+pub use crate::xanterella::oop::result::EventsFailed;
+pub use crate::xanterella::utils::get::User;
+pub use crate::xanterella::utils::get::Paths;
