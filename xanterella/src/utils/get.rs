60b77f49fe3daaab189c5eed0f3ab8d4d2fdc21d
Xeravus <cato.jenisch@gmail.com>
2026-07-29 11:48:37 +0200

    Fix: Add HOME env fallback

--- xanterella/src/utils/get.rs
+++ xanterella/src/utils/get.rs
@@ -1,5 +1,6 @@
 use crate::installer::core::XanterellaInstall;
 use crate::prelude::*;
+use std::env;
 
 pub enum User {
     Root,
@@ -49,7 +50,9 @@ pub trait Get {
 
 impl Get for Xanterella {
     fn get_path(&mut self, path: Paths) -> String {
-        let config = PathBuf::from(&self.home).join(".config").join("xanterella");
+        let home = if self.home.is_empty() { env::var("HOME").expect("Konnte die Home Varialbe nichht extrahieren") } else {
+            self.home.clone() };
+        let config = PathBuf::from(&home).join(".config").join("xanterella");
         let result: PathBuf = match path {
             Paths::Nixconf => self.path.as_str().into(),
             Paths::Config => config,
