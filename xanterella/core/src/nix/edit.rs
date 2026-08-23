fd34045c15ee756d83af9ebf7a5c2200e7e2bef2
Xeravus <cato.jenisch@gmail.com>
2026-08-23 20:51:40 +0200

    Temp

--- xanterella/core/src/nix/edit.rs
+++ xanterella/core/src/nix/edit.rs
@@ -6,6 +6,7 @@ use prolyxena::engine::formater::write::Write;
 
 use crate::prelude::*;
 
+/*
 pub trait Edit {
     fn crylia_edit_start(&mut self) -> Result<(), EventsFailed>;
     fn crylia_add_hardware_conf(&mut self) -> Result<(), EventsFailed>;
@@ -28,7 +29,11 @@ impl<'a> Edit for XanterellaInstall<'a> {
         config.walk_tree().map_err(|err| EventsFailed::Prolyxena(err.to_string()))?;
         Ok(())
     }
+
     fn crylia_add_hardware_link(&mut self, config: &mut FsData) -> Result<(), EventsFailed> {
-        config.insert_from_string
+        let file = config.search_tree("hosts/crylia/configuration.nix").map_err(|err| EventsFailed::Prolyxena(err.to_string()))?;
+        file.insert("imports", "./hardware-configuration.nix").map_err(|err| EventsFailed::Prolyxena(err.to_string()))?;
+        Ok(())
     }
 }
+*/
